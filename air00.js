c52599c8e96e47a109c5cced36d177eabe374c77
Keijur0 <tran.qlam@gmail.com>
2022-10-02 22:31:41 +0200

    air00 done

---
+++ air00.js
@@ -0,0 +1,79 @@
+// This script takes 1 string as argument, and returns it split by spaces
+
+// Functions
+function nbArgCheck(array) 
+{
+    if(nbArg !== 1)
+    {
+        return false;
+    }
+    return true;
+}
+
+function splitString(string_to_cut, separator)
+{
+    arrayOfWords = [];
+    j = 0;
+    for (let i = 0; i < string_to_cut.length; i++)
+    {
+        
+        if (string_to_cut[i] !== separator)
+        {
+            if (!arrayOfWords[j])
+            {
+                arrayOfWords[j] = string_to_cut[i];
+            }
+            else
+            {
+                arrayOfWords[j] = arrayOfWords[j] + string_to_cut[i]; 
+            }
+        }
+        else if (!arrayOfWords[j])
+        {
+            continue;
+        }
+        else
+        {
+            j++;
+        }
+    }
+    return arrayOfWords;
+}
+
+function formatResult(array)
+{
+    var result;
+    for (let i = 0; i < array.length; i++)
+    {
+        if (!result)
+        {
+           result = array[i];
+        }
+        else
+        {
+            result = result + "\n" + array[i];
+        }
+    }
+    return result;
+}
+
+
+
+// Part 1: Parsing
+argList = process.argv.splice(2);
+nbArg = argList.length;
+separator = " ";
+string = argList[0];
+
+// Part 2: Error management
+if (!nbArgCheck(nbArg))
+{
+    console.log("error");
+    return;
+}
+
+// Part 3: Resolution
+listOfWords = splitString(string, separator);
+
+// Part 4: Result display
+console.log(formatResult(listOfWords));
