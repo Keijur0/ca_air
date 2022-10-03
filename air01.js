29d22b921d2a4c5378344b35f00e7d6a2196e227
Keijur0 <tran.qlam@gmail.com>
2022-10-03 09:28:49 +0200

    air01 done

---
+++ air01.js
@@ -0,0 +1,108 @@
+// This script takes 1 string as argument, and returns it split by the last argument.
+
+// Functions
+function nbArgCheck(array) 
+{
+    if(nbArg !== 2)
+    {
+        return false;
+    }
+    return true;
+}
+
+function splitStringWithSpaces(string_to_cut, separator)
+{
+    arrayOfWords = [];
+    j = 0;
+    for (let i = 0; i < string_to_cut.length; i++)
+    {
+        if (string_to_cut[i] !== separator)
+        {
+            if (!arrayOfWords[j])
+            {
+                arrayOfWords[j] = string_to_cut[i];
+            }
+            else
+            {
+                arrayOfWords[j] = arrayOfWords[j] + string_to_cut[i]; 
+            }
+        }
+        else if (!arrayOfWords[j])
+        {
+            continue;
+        }
+        else
+        {
+            j++;
+        }        
+    }
+    return arrayOfWords;
+}
+
+function splitStringWithString (string_to_cut, separator)
+{
+    stringSplitbyString = [];
+    j = 0;
+    for (let i = 0; i < string_to_cut.length; i++)
+    {
+        if (string_to_cut[i] !== separator)
+        {
+          if (!stringSplitbyString[j])
+           {
+                stringSplitbyString[j] = string_to_cut[i];
+           }
+           else
+           {
+                stringSplitbyString[j] = stringSplitbyString[j] + " " + string_to_cut[i];
+           }
+        }
+        else if (!stringSplitbyString)
+        {
+            continue;
+        }
+        else
+        {
+            j++;
+        }
+    }
+
+    return stringSplitbyString;
+}
+
+function formatResult(array)
+{
+    var result;
+    for (let i = 0; i < array.length; i++)
+    {
+        if (!result)
+        {
+           result = array[i];
+        }
+        else
+        {
+            result = result + "\n" + array[i];
+        }
+    }
+    return result;
+}
+
+// Part 1: Parsing
+argList = process.argv.splice(2, process.argv.length - 1);
+nbArg = process.argv.length;
+separator1 = " ";
+separator2 = argList[nbArg - 1];
+string = argList[0];
+
+// Part 2: Error management
+if (!nbArgCheck(nbArg))
+{
+    console.log("error");
+    return;
+}
+
+// Part 3: Resolution
+listOfWords = splitStringWithSpaces(string, separator1);
+finalResult = splitStringWithString(listOfWords, separator2);
+
+// Part 4: Result display
+console.log(formatResult(finalResult));
